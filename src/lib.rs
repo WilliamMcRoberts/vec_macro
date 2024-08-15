13241bce4352c3e01bb33ab94eb6dd073e0f0655
WilliamMcRoberts <wamcroberts2@gmail.com>
2024-08-15 14:48:34 -0400

    fill test

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,26 @@
 #[macro_export]
 macro_rules! avec {
+    // Create Empty Vector
     () => {
         Vec::new()
     };
 
+    // Create Vector With One Or Many Items
     ($($element:expr),+ $(,)?) => {{
         let mut v = Vec::new();
         $(v.push($element);)+
         v
     }};
 
-    // ($element:expr) => {{
-    //     let mut v = Vec::new();
-    //     v.push($element);
-    //     v
-    // }};
+    // Create Vector With Element To Fill Vector "n" Times
+    ($element:expr; $count:expr) => {{
+        let mut v = Vec::new();
+        let x = $element;
+        for _ in 0..$count {
+            v.push(x.clone());
+        }
+        v
+    }};
 }
 
 #[test]
@@ -50,6 +56,31 @@ fn many() {
     assert_eq!(v[1], 45);
     assert_eq!(v[2], 49);
     assert_eq!(v[3], 55);
+}
+
+#[test]
+fn fill() {
+    let v: Vec<i32> = avec![42; 5];
+    assert!(!v.is_empty());
+    assert_eq!(v.len(), 5);
+    assert_eq!(v[0], 42);
+    assert_eq!(v[1], 42);
+    assert_eq!(v[2], 42);
+    assert_eq!(v[3], 42);
+    assert_eq!(v[4], 42);
+}
+
+#[test]
+fn fill_non_literal() {
+    let mut y = Some(42);
+    let v: Vec<i32> = avec![y.take().unwrap(); 5];
+    assert!(!v.is_empty());
+    assert_eq!(v.len(), 5);
+    assert_eq!(v[0], 42);
+    assert_eq!(v[1], 42);
+    assert_eq!(v[2], 42);
+    assert_eq!(v[3], 42);
+    assert_eq!(v[4], 42);
 }
 
 #[test]
